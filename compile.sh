8e96f647cc5df969ec3ed084f554646a8ba06edd
Sven Hwang <sven.chwen@gmail.com>
2022-02-25 12:54:21 -0700

    Add CMake compile script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# mkdir -p build/no_opt
+# pushd build/no_opt
+# cmake -DCMAKE_BUILD_TYPE=Debug ../..
+# make -j
+# popd
+mkdir -p build/O3
+pushd build/O3
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+make -j
+popd
