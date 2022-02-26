9dbb159c3cbe49dfa7c3aa0954fa2571832ef502
Sven Hwang <sven.chwen@gmail.com>
2022-02-25 18:32:29 -0600

    init.sh order fix

--- init.sh
+++ init.sh
@@ -4,13 +4,6 @@ INSTALL_PATH="/p2/installs"
 # install
 sudo apt-get update
 sudo apt-get install -y build-essential wget gcc g++ gdb git vim htop curl autoconf libtool pkg-config fuse libfuse-dev
-## get a newer cmake
-sudo apt-get remove cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.sh
-/bin/sh cmake-3.22.2-linux-x86_64.sh -- --skip-license --prefix=$INSTALL_PATH
-# cp -r cmake-3.22.2-linux-x86_64/* $INSTALL_PATH/
-echo "PATH=$INSTALL_PATH/bin:\$PATH" >> ~/.bashrc
-source ~/.bashrc
 echo "set tabstop=4" > ~/.vimrc
 
 # git
@@ -34,6 +27,14 @@ ssh-add ~/.ssh/id_cs739_afsv1_deploy_key
 sudo mkdir -p /p2
 sudo chown c5h11oh /p2
 mkdir -p $INSTALL_PATH /p2/repo /p2/grpc
+
+## get a newer cmake
+sudo apt-get remove cmake
+wget https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.sh
+/bin/sh cmake-3.22.2-linux-x86_64.sh -- --skip-license --prefix=$INSTALL_PATH
+# cp -r cmake-3.22.2-linux-x86_64/* $INSTALL_PATH/
+echo "PATH=$INSTALL_PATH/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
 git clone git@github.com:c5h11oh/cs739-afsv1.git /p2/repo
 
 # install grpc
@@ -42,7 +43,7 @@ git clone --recurse-submodules -b v1.43.0 https://github.com/grpc/grpc /p2/grpc
 cd /p2/grpc
 mkdir -p cmake/build
 pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
+$INSTALL_PATH/bin/cmake -DgRPC_INSTALL=ON \
       -DgRPC_BUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
       ../..
@@ -54,5 +55,4 @@ popd
 cd /etc/ld.so.conf.d
 echo $INSTALL_PATH | sudo tee p2.conf
 sudo /sbin/ldconfig
-
 
