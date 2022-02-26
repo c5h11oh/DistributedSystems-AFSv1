9f95600cf0e452bfee7c631fc4979382e2a02994
Sven Hwang <sven.chwen@gmail.com>
2022-02-26 16:56:15 -0700

    fuse saturday

--- afs_client.cc
+++ afs_client.cc
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <memory>
 #include <string>
-#include <time.h> // clock_gettime
-#include <limits.h> // LONG_MAX
-#include <unistd.h> // argument parsing
+#include <unistd.h>
 #include <stdlib.h> // malloc
-
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include <grpcpp/grpcpp.h>
 #include "afs.grpc.pb.h"
+#include "afs.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -17,144 +18,271 @@ using grpc::ClientWriter;
 using grpc::Status;
 using namespace cs739;
 
-// #define BUFSIZE 2097152 // 2MB
-#define BUFSIZE 12288 // 12KB
-/* Protobuf ():
-    bytes 	May contain any arbitrary sequence of bytes no longer than 2^32.
-*/
+#define FUSE_USE_VERSION 26
+#define PATH_MAX 4096
+#define AFS_DATA ((struct afs_data_t *) fuse_get_context()->private_data)
 
-class SendFileClient {
-public:
-    SendFileClient(std::shared_ptr<Channel> channel)
-      : stub_(SendFile::NewStub(channel)) {}
+std::unique_ptr<AFS::Stub> stub_;
 
-    SendFileClient(std::shared_ptr<Channel> channel, char* filepath) 
-     : stub_(SendFile::NewStub(channel)), filepath_(filepath) { }
-
-    void send(std::ifstream& file) {
-        ClientContext context;
-        Reply reply;
-        Data d;
-        uint64_t count = 0;
-
-        std::unique_ptr<ClientWriter<Data>> writer(
-            stub_->Send(&context, &reply));
-
-        // read file to buf and send it
-        // https://en.cppreference.com/w/cpp/io/basic_istream/read
-        file.seekg(0, std::ios::end);
-        uint64_t filesize = file.tellg();
-        file.seekg(0);
-        
-        // buffer to be filled with data and sent by gRPC
-        uint64_t bufsize = (uint64_t)BUFSIZE;
-        std::string buf(bufsize, '\0');
-        
-        // array to store all timestamp
-        struct timespec tss[(10 + filesize / bufsize)];
-        // timestamps = (struct timespec*)malloc(sizeof(struct timespec) * (10 + filesize / bufsize));
-        ts_ptr = timestamps = tss;
-
-        clock_gettime(CLOCK_MONOTONIC, ts_ptr++); // start
-        while (file.read(&buf[0], bufsize)) {
-            d.set_b(buf);
-            if (!writer->Write(d))
-                break;
-            count += d.b().size();
-            clock_gettime(CLOCK_MONOTONIC, ts_ptr++);
-        }
-        if (file.eof()) {
-            buf.resize(file.gcount());
-            d.set_b(buf);
-            writer->Write(d);
-            count += d.b().size();
-        }
-        writer->WritesDone();
-        clock_gettime(CLOCK_MONOTONIC, ts_ptr++); // end
-
-        Status status = writer->Finish();
-        if (status.ok()) {
-            // clock_gettime(CLOCK_MONOTONIC, &u);
-            // std::cout << "Success sending data. stop timer and checksum...\n";
-            std::stringstream md5cmd; 
-            md5cmd << "md5sum " << filepath_;
-            // std::string c_md5 = exec(md5cmd.str().c_str()).substr(0, 32);
-            std::string s_md5 = reply.md5sum();
-            // std::cout << "client: filesize=" << filesize << ", md5sum=" << c_md5 << "\n";
-            // std::cout << "server: filesize=" << reply.r() << ", md5sum=" << s_md5 << "\n";
-            // if (c_md5 != s_md5 || filesize != reply.r()) {
-                // std::cout << "Fail to send correct data.\n";
-                // goto cleanup;
-            // }
-            measure_bandwidth(bufsize, buf.size());
-        } else {
-            std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
-            std::cout << "Fail to send file\n";
-            goto cleanup;
-        }
-cleanup:
-        // free(timestamps);
-        return;
-    }
-
-private:
-    std::unique_ptr<SendFile::Stub> stub_;
-    char* filepath_;
-    struct timespec* timestamps, * ts_ptr;
-    void measure_bandwidth(int usual_size, int last_size) {
-        std::cout << "Measure bandwidth:\n" <<
-                     "size\tduration (ns)\tbandwidth (Bps)\n";
-        int count = (ts_ptr - timestamps) - 1;
-        int i;
-        for (i = 0; i < count; ++i) {
-            struct timespec* t = (timestamps + i), *u = (timestamps + i + 1);
-            long dur_ns = (u->tv_nsec - t->tv_nsec) + (u->tv_sec - t->tv_sec) * 1000000000;
-            double Bps = ((i == count - 1) ? last_size : usual_size) * 1.0 / (dur_ns / 1e9);
-            // std::cout << ((i == count - 1) ? last_size : usual_size) << "\t" << dur_ns << "\t" << Bps << "\n";
-            printf("%7d\t%12ld\t%11.1f\n", ((i == count - 1) ? last_size : usual_size), dur_ns, Bps);
-        }
-        
-        int size_total = usual_size * (count - 1) + last_size;
-        long dur_total_ns = (ts_ptr - 1)->tv_nsec - timestamps->tv_nsec + 
-                            ((ts_ptr - 1)->tv_sec - timestamps->tv_sec) * 1000000000;
-        double Bps_total = size_total * 1.0 / (dur_total_ns / 1e9);
-        std::cout << "------------------ Total ------------------\n" <<
-                     size_total << "\t" << dur_total_ns << "\t" << Bps_total << "\n";
-    }
+struct afs_data_t {
+    std::string cache_path;
+    std::unique_ptr<AFS::Stub> stub_;
 };
 
-void print_usage(char* proc_name = nullptr) {
-    std::cout << (proc_name ? proc_name : "send_file_client") << " [-s <server_ip:port>] <file_to_be_send>\n";
+// int afs_access(const char *path, int mask)
+// {
+//     int retstat = 0;
+//     char fpath[PATH_MAX];
+   
+//     bb_fullpath(fpath, path);
+    
+//     retstat = access(fpath, mask);
+    
+//     if (retstat < 0)
+// 	retstat = log_error("bb_access access");
+    
+//     return retstat;
+// }
+
+// int afs_opendir(const char *path, struct fuse_file_info *fi)
+// {
+//     DIR *dp;
+//     int retstat = 0;
+//     char fpath[PATH_MAX];
+    
+//     bb_fullpath(fpath, path);
+
+//     // since opendir returns a pointer, takes some custom handling of
+//     // return status.
+//     dp = opendir(fpath);
+//     log_msg("    opendir returned 0x%p\n", dp);
+//     if (dp == NULL)
+// 	retstat = log_error("bb_opendir opendir");
+    
+//     fi->fh = (intptr_t) dp;
+    
+//     log_fi(fi);
+    
+//     return retstat;
+// }
+
+// int afs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+// 	       struct fuse_file_info *fi)
+// {
+//     int retstat = 0;
+//     DIR *dp;
+//     struct dirent *de;
+    
+//     log_msg("\nbb_readdir(path=\"%s\", buf=0x%08x, filler=0x%08x, offset=%lld, fi=0x%08x)\n",
+// 	    path, buf, filler, offset, fi);
+//     // once again, no need for fullpath -- but note that I need to cast fi->fh
+//     dp = (DIR *) (uintptr_t) fi->fh;
+
+//     // Every directory contains at least two entries: . and ..  If my
+//     // first call to the system readdir() returns NULL I've got an
+//     // error; near as I can tell, that's the only condition under
+//     // which I can get an error from readdir()
+//     de = readdir(dp);
+//     log_msg("    readdir returned 0x%p\n", de);
+//     if (de == 0) {
+// 	retstat = log_error("bb_readdir readdir");
+// 	return retstat;
+//     }
+
+//     // This will copy the entire directory into the buffer.  The loop exits
+//     // when either the system readdir() returns NULL, or filler()
+//     // returns something non-zero.  The first case just means I've
+//     // read the whole directory; the second means the buffer is full.
+//     do {
+// 	log_msg("calling filler with name %s\n", de->d_name);
+// 	if (filler(buf, de->d_name, NULL, 0) != 0) {
+// 	    log_msg("    ERROR bb_readdir filler:  buffer full");
+// 	    return -ENOMEM;
+// 	}
+//     } while ((de = readdir(dp)) != NULL);
+    
+//     log_fi(fi);
+    
+//     return retstat;
+// }
+
+// int afs_open(const char *path, struct fuse_file_info *fi)
+// {
+//     int retstat = 0;
+//     int fd;
+//     char fpath[PATH_MAX];
+    
+//     log_msg("\nbb_open(path\"%s\", fi=0x%08x)\n",
+// 	    path, fi);
+//     bb_fullpath(fpath, path);
+    
+//     // if the open call succeeds, my retstat is the file descriptor,
+//     // else it's -errno.  I'm making sure that in that case the saved
+//     // file descriptor is exactly -1.
+//     fd = log_syscall("open", open(fpath, fi->flags), 0);
+//     if (fd < 0)
+// 	retstat = log_error("open");
+	
+//     fi->fh = fd;
+
+//     log_fi(fi);
+    
+//     return retstat;
+// }
+
+// int afs_flush(const char *path, struct fuse_file_info *fi)
+// {
+//     log_msg("\nbb_flush(path=\"%s\", fi=0x%08x)\n", path, fi);
+//     // no need to get fpath on this one, since I work from fi->fh not the path
+//     log_fi(fi);
+	
+//     return 0;
+// }
+
+// int afs_release(const char *path, struct fuse_file_info *fi)
+// {
+//     log_msg("\nbb_release(path=\"%s\", fi=0x%08x)\n",
+// 	  path, fi);
+//     log_fi(fi);
+
+//     // We need to close the file.  Had we allocated any resources
+//     // (buffers etc) we'd need to free them here as well.
+//     return log_syscall("close", close(fi->fh), 0);
+// }
+
+// int afs_mknod(const char *path, mode_t mode, dev_t dev)
+// {
+//     int retstat;
+//     char fpath[PATH_MAX];
+    
+//     log_msg("\nbb_mknod(path=\"%s\", mode=0%3o, dev=%lld)\n",
+// 	  path, mode, dev);
+//     bb_fullpath(fpath, path);
+    
+//     // On Linux this could just be 'mknod(path, mode, dev)' but this
+//     // tries to be be more portable by honoring the quote in the Linux
+//     // mknod man page stating the only portable use of mknod() is to
+//     // make a fifo, but saying it should never actually be used for
+//     // that.
+//     if (S_ISREG(mode)) {
+// 	retstat = log_syscall("open", open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode), 0);
+// 	if (retstat >= 0)
+// 	    retstat = log_syscall("close", close(retstat), 0);
+//     } else
+// 	if (S_ISFIFO(mode))
+// 	    retstat = log_syscall("mkfifo", mkfifo(fpath, mode), 0);
+// 	else
+// 	    retstat = log_syscall("mknod", mknod(fpath, mode, dev), 0);
+    
+//     return retstat;
+// }
+
+// int afs_unlink(const char *path)
+// {
+//     char fpath[PATH_MAX];
+    
+//     log_msg("bb_unlink(path=\"%s\")\n",
+// 	    path);
+//     bb_fullpath(fpath, path);
+
+//     return log_syscall("unlink", unlink(fpath), 0);
+// }
+
+int afs_getattr(const char *path, struct stat *stbuf)
+{
+    grpc::ClientContext context;
+    cs739::Filepath request;
+    cs739::StatContent response;
+    request.set_filepath(std::string(path));
+	AFS_DATA->stub_->Stat(&context, request, &response);
+    
+    int res = 0;
+
+	memset(stbuf, 0, sizeof(struct stat));
+	if (strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+	} else if (strcmp(path, hello_path) == 0) {
+		stbuf->st_mode = S_IFREG | 0444;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen(hello_str);
+	} else
+		res = -ENOENT;
+
+	return res;
 }
 
-int main(int argc, char** argv) {
-    std::string server_addr = "128.105.37.149:53706"; // royal-09=128.105.37.149:53706; royal-##=128.105.37.(140+##)
+// int afs_mkdir(const char *path, mode_t mode)
+// {
+//     char fpath[PATH_MAX];
+    
+//     log_msg("\nbb_mkdir(path=\"%s\", mode=0%3o)\n",
+// 	    path, mode);
+//     bb_fullpath(fpath, path);
 
-    int opt;
-    while ((opt = getopt(argc, argv, "hs:")) != -1) {
-        switch (opt) {
-            case 's':
-                server_addr = optarg;
-                break;
-            case 'h':
-                print_usage(argv[0]);
-                exit(0);
-            case '?':
-            default:
-                std::cerr << argv[0] << ": invalid argument\n";
-                print_usage(argv[0]);
-                exit(1);
-        }
-    }
-    if (optind >= argc) {
-        std::cerr << argv[0] << ": specify file\n";
-        print_usage(argv[0]);
-        exit(1);
-    }
-    std::ifstream file(argv[optind], std::ios::binary);
-    SendFileClient client(grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials()), argv[optind]);
-    client.send(file);
-    file.close();
+//     return log_syscall("mkdir", mkdir(fpath, mode), 0);
+// }
 
-    return 0;
+// int afs_rmdir(const char *path)
+// {
+//     char fpath[PATH_MAX];
+    
+//     log_msg("bb_rmdir(path=\"%s\")\n",
+// 	    path);
+//     bb_fullpath(fpath, path);
+
+//     return log_syscall("rmdir", rmdir(fpath), 0);
+// }
+
+// int afs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+// {
+//     int retstat = 0;
+    
+//     log_msg("\nbb_read(path=\"%s\", buf=0x%08x, size=%d, offset=%lld, fi=0x%08x)\n",
+// 	    path, buf, size, offset, fi);
+//     // no need to get fpath on this one, since I work from fi->fh not the path
+//     log_fi(fi);
+
+//     return log_syscall("pread", pread(fi->fh, buf, size, offset), 0);
+// }
+
+// int afs_write(const char *path, const char *buf, size_t size, off_t offset,
+// 	     struct fuse_file_info *fi)
+// {
+//     int retstat = 0;
+    
+//     log_msg("\nbb_write(path=\"%s\", buf=0x%08x, size=%d, offset=%lld, fi=0x%08x)\n",
+// 	    path, buf, size, offset, fi
+// 	    );
+//     // no need to get fpath on this one, since I work from fi->fh not the path
+//     log_fi(fi);
+
+//     return log_syscall("pwrite", pwrite(fi->fh, buf, size, offset), 0);
+// }
+
+static struct fuse_operations afs_oper = {
+	// .access		= afs_access,
+	.opendir	= afs_opendir,
+	.readdir	= afs_readdir,
+	.open		= afs_open,
+	.flush		= afs_flush,
+	.release	= afs_release,
+	.mknod		= afs_mknod,
+	.unlink		= afs_unlink,
+	.getattr	= afs_getattr,
+	.mkdir		= afs_mkdir,
+	.rmdir		= afs_rmdir,
+	.read		= afs_read,
+	.write		= afs_write, 
+};
+
+int main(int argc, char *argv[])
+{
+    std::string server_addr = "127.0.0.1:53706";
+    ;
+    afs_data_t* afs_data = new afs_data_t;
+	afs_data->stub_ = AFS::NewStub(grpc::CreateChannel(server_addr, grpc::InsecureChannelCredentials()));
+    afs_data->cache_path = argv[1]; // CHECK!
+
+    return fuse_main(argc, argv, &afs_oper, afs_data);
 }
