c1a2e05a7560c9387118968c380f5cbc2ddda51f
Akhil <vboddupalli@wisc.edu>
2022-03-04 15:55:56 -0700

    add driver write in measurement

--- measurement/driver_write.cc
+++ measurement/driver_write.cc
@@ -1,23 +1,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string>
 #include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
-#include <string.h>
+#include "timespec_lib.h"
 
 int main(int argc, char *argv[])
 {
-    int fd;
-    char buf[256] = "foo bar";
+    std::string buf(atoi(argv[2]), '0'); 
 
+    int fd;
+    struct timespec start, end, open_diff, close_diff;
+    long open_ns, close_ns;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    fd = open(argv[1], O_TRUNC);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    sub_timespec(start, end, &open_diff);
+    open_ns = open_diff.tv_sec * NS_PER_SECOND + open_diff.tv_nsec;
 
     if (fd < 0) {
-        std::cout << "open failed\n";
+        perror("open");
         exit(1);
     }
 
-    write(fd, buf, strlen(buf));
+
+    write(fd, buf.c_str(), buf.size());
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    close(fd);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    sub_timespec(start, end, &close_diff);
+    close_ns = close_diff.tv_sec * NS_PER_SECOND + close_diff.tv_nsec;
+
+    std::cout << "open,close\n";
+    std::cout << open_ns << "," << close_ns << std::endl;
 
     return 0;
 }
